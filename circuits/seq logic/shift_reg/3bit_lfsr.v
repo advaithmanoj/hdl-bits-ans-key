9c86e720bba89ed5a1b13fbaf7a2c6f3158ca838
Advaith Manoj <88617390+advaithmanoj@users.noreply.github.com>
2026-06-10 00:31:26 +0530

    Update 3bit_lfsr.v

--- circuits/seq logic/shift_reg/3bit_lfsr.v
+++ circuits/seq logic/shift_reg/3bit_lfsr.v
@@ -1,1 +1,16 @@
+//same like the eariler lfsr
+
+module top_module (
+	input [2:0] SW,      // R
+	input [1:0] KEY,     // L and clk
+	output [2:0] LEDR);  // Q
+wire tap;
+    always@(posedge KEY[0])begin
+        if (KEY[1]) 
+            LEDR <= SW;
+        else
+            LEDR <= {tap,LEDR[0],LEDR[2]};
+    end
+    assign tap = LEDR[2]^LEDR[1];
+endmodule
 
